99ef4c4adc408dc53f749f4fc89a444f36427a9c
Matheus Valle <matheusvalleguitar@gmail.com>
2021-09-08 14:12:08 -0300

    Finalizando projeto Chuck Norris Jokes

--- src/components/Giphy/index.js
+++ src/components/Giphy/index.js
@@ -5,7 +5,13 @@ export default function Giphy() {
   const [gif, setGif] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.giphy.com/v1/gifs/random?api_key=p2AQOsLpmPprm8cUeAIYQHpD0u5VpCfu&tag=chuck+norris&rating=g')
+    axios.get('https://api.giphy.com/v1/gifs/random', {
+      params: {
+        "api_key": "p2AQOsLpmPprm8cUeAIYQHpD0u5VpCfu",
+        "tag": "chuck norris",
+        "rating": "g"
+      }
+    })
       .then(res => {
         console.log(res.data.image_url)
         setGif(res.data.data.image_url);
